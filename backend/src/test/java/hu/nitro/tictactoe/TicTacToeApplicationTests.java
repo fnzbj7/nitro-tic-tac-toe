05eaea259eee1cef718b55aea6bebbb165a02205
Árpás Milán <jukijun91@gmail.com>
2021-02-13 10:08:44 +0100

    Creating backend and frontend template

---
+++ backend/src/test/java/hu/nitro/tictactoe/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nitro.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
